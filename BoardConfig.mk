6c52d8c378cffb631a13a85baebf849f8d5d7159
Vachounet <vachounet@live.fr>
2020-03-06 16:14:46 +0100

    doha: set kernel offset
    and update vbmeta flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,8 +22,6 @@ DEVICE_PATH := device/motorola/doha
 
 include $(DEVICE_PATH)/BoardConfigGsi.mk
 
-
-
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RECOVERY := false
 
@@ -52,6 +50,7 @@ TARGET_COMPILE_WITH_MSM_KERNEL := true
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 firmware_class.path=/vendor/firmware_mnt/image earlycon=msm_geni_serial,0x4a90000 loop.max_part=7 cgroup.memory=nokmem,nosocket androidboot.usbcontroller=4e00000.dwc3 printk.devkmsg=on androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
@@ -125,7 +124,8 @@ BOARD_VNDK_VERSION := current
 PRODUCT_EXTRA_VNDK_VERSIONS := 28
 TARGET_NO_KERNEL := false
 BOARD_AVB_ENABLE := true
-BOARD_BUILD_DISABLED_VBMETAIMAGE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
 BOARD_USES_VENDORIMAGE := true
 
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
