7e5d9e1e27d035d9c957919830723604ef9f927f
qaøtyk <56375579+qaotyk@users.noreply.github.com>
2020-05-26 00:54:31 -0500

    Add AOSP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/device.mk \
-    $(LOCAL_DIR)/omni_doha.mk
+    $(LOCAL_DIR)/aosp_doha.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_doha-user \
-    omni_doha-userdebug \
-    omni_doha-eng \
+    aosp_doha-user \
+    aosp_doha-userdebug \
+    aosp_doha-eng \
